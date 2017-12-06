3b09ac57b53b74d88b178cbb8f337b36123345b5
polinalester <epikh1gh@yandex.ru>
2017-12-06 14:10:49 +0300

    Updated HexToDec Converter

--- java.intro/Conversion.java
+++ java.intro/Conversion.java
@@ -13,12 +13,43 @@ import java.util.*;
 
 public class Conversion {
     public static void main(String args[]){
-        Scanner reader = new Scanner(System.in);
-        System.out.println("Enter hexadecimal:");
-        String hex;
-        hex = reader.nextLine();
-        System.out.println(" => decimal:");
-        long dec = Long.parseLong(hex, 16);
-        System.out.println(dec);
+       Scanner reader = new Scanner(System.in);
+       System.out.println("Enter hex number: ");
+       String hex = reader.nextLine();
+       while(!hex.matches("[-+]?[A-Fa-f0-9]+")){
+           System.out.println("Enter HEX number: ");
+           hex=reader.nextLine();
+       }
+       int dec = 0;
+       boolean negative = false;
+       if(hex.charAt(0)=='-')
+       {
+           negative=true;
+           hex=hex.substring(1);
+       }
+       else if(hex.charAt(0)=='+')
+           hex=hex.substring(1);
+       
+       int n = hex.length();
+       char letter;
+       for (int i=n-1;i>-1;i--)
+       {
+           letter = hex.charAt(i);
+           if(letter <= '9')
+           {
+               dec+=Character.getNumericValue(letter)*Math.pow(16,n-i-1);
+           }
+           else if(letter>'F')
+           {
+               dec+=(letter-87)*Math.pow(16,n-i-1);
+           }
+           else{
+               dec+=(letter-55)*Math.pow(16,n-i-1);
+           }
+           
+       }
+      if(negative)
+          System.out.println(hex+" => "+(-dec));
+      else System.out.println(hex+" => "+dec);
     }
 }
